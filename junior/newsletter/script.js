b9f8fc9728678d3735045442ce7936767c0d83b2
Lucas Dias Leandro <147958304+lucasserdev@users.noreply.github.com>
2023-11-20 14:04:00 -0300

    finalizado, porém falta um detalhe
    quando vai diminuir ou aumentar a tela depois de ter colocado o email da um bug visual e tem que atualizar a tela pra normalizar

--- junior/newsletter/script.js
+++ junior/newsletter/script.js
@@ -1,7 +1,8 @@
+let sub = document.getElementById('sub')
+
 function clicar() {
     let emailInput = document.getElementById('iemail')
     let msgInvalid = document.getElementById('error')
-    let sub = document.getElementById('sub')
     let principal = document.getElementById('principal')
     let myemail = document.getElementById('myemail')
 
@@ -19,5 +20,14 @@ function clicar() {
         principal.style.display = 'none'
         myemail.innerHTML = `<strong>${email}</strong>`
     }
-    
+}
+
+function voltar() {
+    let larguraTela = window.innerWidth
+    sub.style.display = 'none'
+    if (larguraTela < 992) {
+        principal.style.display = 'block'
+    } else {
+        principal.style.display = 'flex'
+    }
 }
